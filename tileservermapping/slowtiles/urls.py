59e5a52c378fc9d9ff7c527dcc1ac4abd0570153
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2020-02-10 23:02:21 +0100

    add upload of generated tiles

--- tileservermapping/slowtiles/urls.py
+++ tileservermapping/slowtiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import insert_tile, get_server_list
+from .views import insert_tile, get_server_list, upload_tile
 
 urlpatterns = [
     path('add/<int:z>/<int:x>/<int:y>', insert_tile, name="insert_tile"),
-    path('server_list/<int:z>/<int:x>/<int:y>', get_server_list, name="server_list")
+    path('server_list/<int:z>/<int:x>/<int:y>', get_server_list, name="server_list"),
+    path('upload_tile/<int:z>/<int:x>/<int:y>.<slug:format>', upload_tile, name='upload_tile'),
 ]
--- tileservermapping/slowtiles/views.py
+++ tileservermapping/slowtiles/views.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
+from django.conf import settings
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 from tileservermapping.mapping.models import Server
 from tileservermapping.slowtiles.models import SlowTiles
@@ -18,3 +23,24 @@ def get_server_list(request, z, x, y):
     for tile in tiles:
         answer += "{}/{}/{}\n".format(tile.z, tile.x, tile.y)
     return HttpResponse(answer, content_type='text')
+
+
+def handle_uploaded_file(path, f):
+    with open(path, 'wb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+@require_POST
+@csrf_exempt
+def upload_tile(request, z, x, y, format):
+    # TODO auth oth server
+    # TODO check if tile in server
+    if not SlowTiles.objects.filter(x=x, y=y, z=z, count__gte=5).count():
+        return HttpResponse('Tile should not be static\n', content_type="text", status=403)
+    path = Path(settings.TILE_WEBSERVER_DIR) / str(z) / str(x)
+    if not path.exists():
+        path.mkdir(parents=True)
+    file = path / "{}.{}".format(y, format)
+    existed = file.exists()
+    handle_uploaded_file(file, request.FILES['tile'])
+    return HttpResponse('Updated\n' if existed else 'OK\n', content_type='text')
