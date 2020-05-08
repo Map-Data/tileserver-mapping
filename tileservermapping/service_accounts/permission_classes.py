a27f4e73c4cdf7ad7c44c2129544cdf568290f8b
Finn-Thorben Sell <dev@finn-thorben.me>
2020-05-08 11:50:50 +0200

    add additional service-account permission classes

--- tileservermapping/service_accounts/permission_classes.py
+++ tileservermapping/service_accounts/permission_classes.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 
+from . import models
+
 
 class RequestAuthToItself(BasePermission):
     """
@@ -8,3 +10,24 @@ class RequestAuthToItself(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.auth is not None and request.auth == obj
+
+
+class RequestAuthIsManagingThis(BasePermission):
+    """
+    Grants access to an object if the objects `managed_by` property is a foreign key that points to the
+    `request.auth` object
+    """
+    def has_object_permission(self, request, view, obj):
+        return hasattr(obj, 'managed_by') and obj.managed_by == request.auth
+
+
+class IsServiceAccountAuthenticated(BasePermission):
+    """
+    Grants access when the request is authenticated by a service-account
+    """
+    def has_permission(self, request, view):
+        return request.auth is not None and isinstance(request.auth, models.ServiceAccount)
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
